554b0d87c3ecc9b95f0263e994d41982bb0d172d
stephane71 <stephanemaire71@gmail.com>
2020-01-31 15:19:37 +0100

    Get | remove usage of callback argument

--- places/get.js
+++ places/get.js
@@ -1,43 +1,31 @@
-require("dotenv").config({ path: `.env.${process.env.NODE_ENV}` });
-
-const now = require("../utils/now");
 const DynamoDB = require("../DynamoDB");
 const placesDB = new DynamoDB();
 
-module.exports.handler = async (event = {}, context, callback) => {
-  now();
-  console.log("[La Foulee] Get a place");
-
+module.exports.handler = async (event = {}) => {
   const { slug } = event.pathParameters;
 
   if (!slug) {
-    callback(null, {
+    return {
       statusCode: 400,
       body: JSON.stringify({ message: "Invalid request" })
-    });
-    return;
+    };
   }
 
-  let place = null;
   try {
-    place = await placesDB.getPlace(slug);
+    const place = await placesDB.getPlace(slug);
+    if (!place) {
+      return {
+        statusCode: 404
+      };
+    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(place)
+    };
   } catch (e) {
     console.log(e);
-    callback(null, {
+    return {
       statusCode: 500
-    });
-    return;
+    };
   }
-
-  if (!place) {
-    callback(null, {
-      statusCode: 404
-    });
-    return;
-  }
-
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(place)
-  });
 };
